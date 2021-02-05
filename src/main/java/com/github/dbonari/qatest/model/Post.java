2ce8e1f8269f8d44e2519c5ca3b9940a8f8b5732
dbonari <denis.bonari@endava.com>
2021-02-05 17:37:30 +0200

    Fix coverage errors in model classes

--- src/main/java/com/github/dbonari/qatest/model/Post.java
+++ src/main/java/com/github/dbonari/qatest/model/Post.java
@@ -3,21 +3,8 @@ package com.github.dbonari.qatest.model;
 public class Post {
 
     private int id;
-    private int userId;
 
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
     }
 }
--- src/main/java/com/github/dbonari/qatest/model/User.java
+++ src/main/java/com/github/dbonari/qatest/model/User.java
@@ -9,15 +9,7 @@ public class User {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getUsername() {
         return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 }
